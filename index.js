7a8634556bc7dc2b4830d8b9bd0058477075918b
totoMen <106345771+totoMen@users.noreply.github.com>
2022-05-28 11:46:18 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const alumno = [
+    nombre = 'Ignacio Mendivil',
+    edad = 17,
+    soyDesarollador = true,
+    fechaDeNacimiento = new Date("june 11 2004"),    
+    libroFavorito = {
+        titulo: 'The Psychology of Money: Timeless lessons on wealth, greed, and happiness',
+        autor: 'Morgan Housel',
+        fecha: new Date("september 8 2020"),
+        URL: 'https://www.amazon.com/-/es/Morgan-Housel/dp/0857197681' 
+    }
+];
+
+console.log(alumno);
+
